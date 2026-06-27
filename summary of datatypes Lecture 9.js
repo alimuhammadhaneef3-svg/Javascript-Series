230ca865d7bdea689cd61c5b2ebb15b9f6aa60bb
haneef <alimuhammadhaneef3@gmail.com>
2026-06-27 22:15:12 +0500

    Added summary of datatypes lecture 9

---
+++ summary of datatypes Lecture 9.js
@@ -0,0 +1,34 @@
+const id= Symbol("id12")
+const id3= Symbol("id12")
+
+console.log(id==id3);
+
+array= ["ali",'rehman','haneef']
+console.log(array[array.length-1]);
+
+myobj = {
+    name : 'Haneef',
+    age : 20,
+    class : 4,
+    Blood : 'A+'
+
+
+}
+
+console.log(myobj.name);
+myobj.name = "Asghar"
+
+
+console.log(myobj.name);
+
+console.log(typeof myobj);
+
+myfunc = function(){
+    console.log("I am King");
+    
+}
+
+console.log(myfunc);
+
+
+
