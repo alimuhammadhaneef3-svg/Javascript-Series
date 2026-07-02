b67f76c3198c0f6d010ec9121973eeba5d0fede1
haneef <alimuhammadhaneef3@gmail.com>
2026-07-02 23:29:05 +0500

    Updated date & time lecture file

--- date & time lec13.js
+++ date & time lec13.js
@@ -1,25 +1,25 @@
-// let mydate = new Date()
-// console.log(mydate.toString());
-// console.log(mydate.getDate());
-// console.log(mydate.toDateString());
-// console.log(mydate.toLocaleString());
-// console.log(mydate.toLocaleDateString());
-// console.log(mydate.getFullYear());
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.getDate());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.getFullYear());
 
 const { IoLogoNpm } = require("react-icons/io");
 
 
-// let calculatedate = new Date(2026,6,2)
-// console.log(calculatedate.toDateString());
+let calculatedate = new Date(2026,6,2)
+console.log(calculatedate.toDateString());
 
 
-// let Timestamp = Date.now()
-// console.log(Timestamp);
-// console.log(typeof Timestamp);
+let Timestamp = Date.now()
+console.log(Timestamp);
+console.log(typeof Timestamp);
 
-// console.log(calculatedate.getTime());
+console.log(calculatedate.getTime());
 
-// console.log(Math.floor(Date.now()/1000)); //converted into Seconds
+console.log(Math.floor(Date.now()/1000)); //converted into Seconds
 
 
 let newdate =new Date()
