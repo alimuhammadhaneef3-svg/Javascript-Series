646915b0c432e34ea1c0b05a318b12f0cb278a68
haneef <alimuhammadhaneef3@gmail.com>
2026-07-03 15:36:30 +0500

    Added more array examples and methods

--- arrays lec 14.js
+++ arrays lec 14.js
@@ -1,35 +1,35 @@
-// let myarray= [12,34,55,66,77,98]
-// myarray.push(31)
-// myarray.pop()
-// console.log(myarray);
-// myarray.pop()
-// myarray.unshift(100)
-// console.log(myarray);
-// myarray.shift(13)
-// console.log(myarray.includes(7));
+let myarray= [12,34,55,66,77,98]
+myarray.push(31)
+myarray.pop()
+console.log(myarray);
+myarray.pop()
+myarray.unshift(100)
+console.log(myarray);
+myarray.shift(13)
+console.log(myarray.includes(7));
 
-// // myarray.splice(2,1)
+// myarray.splice(2,1)
 
-// console.log(myarray);
+console.log(myarray);
 
-// const newone= myarray.join()
-// console.log(newone);
-// console.log(typeof newone);
-
-
-// //Slice & Splice
-
-// let arrt = [10,20,30,40,50,60]
-// console.log(arrt.slice(1,4)); //start from index 1 and goes to 4th index but exclude it
-// console.log(arrt);
-
-// //splice
-// console.log(arrt.splice(2,3));//start with index 2 and delete next 3 elemnets it change original array too
-// console.log(arrt);
+const newone= myarray.join()
+console.log(newone);
+console.log(typeof newone);
 
 
-// // newsplice= arrt.splice(2,1)
-// // console.log(newsplice);
+//Slice & Splice
+
+let arrt = [10,20,30,40,50,60]
+console.log(arrt.slice(1,4)); //start from index 1 and goes to 4th index but exclude it
+console.log(arrt);
+
+//splice
+console.log(arrt.splice(2,3));//start with index 2 and delete next 3 elemnets it change original array too
+console.log(arrt);
+
+
+// newsplice= arrt.splice(2,1)
+// console.log(newsplice);
 
 
 
