cebb362ea8b79135ee744df519e4e170131af22c
haneef <alimuhammadhaneef3@gmail.com>
2026-07-10 13:29:10 +0500

    Made latest changes to JavaScript files

--- local vs global fuction.js
+++ local vs global fuction.js
@@ -1,40 +1,40 @@
-// // let c=900
-// // if (true)
-// // {
-// //     let a =10
-// //     var b =20
-// //     const c=30
+let c=900
+if (true)
+{
+    let a =10
+    var b =20
+    const c=30
    
    
 
-// // }
-// // console.log(c);
+}
+console.log(c);
 
-// function one (){
-//     const user = "Haneeef"
+function one (){
+    const user = "Haneeef"
    
     
-// function two(){
-//     const website = 'Haneef Rajo'
-//     console.log(website);
-//     console.log(user);
+function two(){
+    const website = 'Haneef Rajo'
+    console.log(website);
+    console.log(user);
     
     
-// }
-// two()
-// }
-// one()
+}
+two()
+}
+one()
 
 
-// if (true){
-//  const name = "Haneef"
-//  if (name === "Haneef"){
-//     const website = "youtube"
-//      console.log(name + website); 
-//  }
+if (true){
+ const name = "Haneef"
+ if (name === "Haneef"){
+    const website = "youtube"
+     console.log(name + website); 
+ }
 
     
-// }
+}
 console.log(addone(12));
 function addone (nums){
     return nums+4
@@ -43,7 +43,7 @@ function addone (nums){
 
 
 
-console.log( addtwo(9));
+onsole.log( addtwo(9));
 const addtwo = function (num){
     return num+3
 }
